a5ef02cab69d16c4ed8e8149889ac1ee9328b262
veronika sukantha <85723282+veronika-sukantha@users.noreply.github.com>
2023-07-25 15:35:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sunrise(){
+    document.getElementById("sun").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ21bgzs_j5NOvDsjA-WvzbkAltfqB8EZTbjsMZpzvq8Q&s";
+}
+function sunset(){
+    document.getElementById("sun").src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Cape_may.jpg/640px-Cape_may.jpg";
+}
+function sunimage(){
+    document.getElementById("sun").src="https://media.istockphoto.com/id/993738504/photo/hot-summer-or-heat-wave-background-orange-sky-with-glowing-sun.jpg?b=1&s=612x612&w=0&k=20&c=k2FVI6ZCv_aQgLF6WmHeSEsQBpOokWh66aqj7lO3qxE=";
+}
